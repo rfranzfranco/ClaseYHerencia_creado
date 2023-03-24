f4fd0b9ee538e94ebef11ff9e9623d7f5b38d6f4
Sergio Diaz <rfranz188@gmail.com>
2023-03-24 00:37:16 -0400

    Clase auxiliar agregada

---
+++ ClasesHerencia/Auxiliar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesHerencia
+{
+    internal class Auxiliar
+    {
+        public decimal sueldo { get; set; }
+        public int Hora { get; set; }
+    }
+}
